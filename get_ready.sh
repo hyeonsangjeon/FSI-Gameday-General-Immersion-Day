5b788e6aa9c988c4b4886fa227322cecb8c3f644
Hyeon Sang Jeon <wingnut0310@gmail.com>
2022-04-13 12:19:41 +0900

    Update get_ready.sh
    doesn't use 8080 in office

--- get_ready.sh
+++ get_ready.sh
@@ -1,4 +1,4 @@
  #/bin/bash
- docker run -d --name model-api -p8080:5000 modenaf360/sentiment-detector
+ docker run -d --name model-api -p8088:5000 modenaf360/sentiment-detector
 
  docker logs -f model-api
