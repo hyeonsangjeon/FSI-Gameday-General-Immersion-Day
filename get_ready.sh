d94b0f3a624674b3c4e30460f62f014ffb8ea089
Hyeon Sang Jeon <wingnut0310@gmail.com>
2022-12-13 00:36:23 +0900

    Update get_ready.sh

--- get_ready.sh
+++ get_ready.sh
@@ -1,4 +1,4 @@
  #/bin/bash
-sudo docker run -d --name model-api -p80:5000 modenaf360/sentiment-detector
+sudo docker run -d --name model-api -p8080:5000 modenaf360/sentiment-detector
 
 sudo docker logs -f model-api
